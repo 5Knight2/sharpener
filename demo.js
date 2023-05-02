b31888635e77afc3025baa3cbb5c1113f30a22b6
5Knight2 <pratiktarale258@gmail.com>
2023-05-02 22:42:47 +0530

    DOM-practical

--- demo.js
+++ demo.js
@@ -1,18 +1,34 @@
-
-let items=document.querySelector('#items');
-let h=document.createElement('div');
-
-let p1=document.createElement('li');
-p1.className='list-group-item';
-let txt=document.createTextNode('Hello');
-let txt2=document.createTextNode('Hello');
-
-p1.appendChild(txt);
-h.appendChild(txt2);
+let form=document.querySelector('#addForm');
+let ulist=document.querySelector('ul');
 
 
-let li=document.querySelector('ul');
-li.parentElement.insertBefore(p1,li);
+form.addEventListener('submit',addlist);
+ulist.addEventListener('click',removelist);
 
-let h1=document.querySelector('h1');
-h1.parentElement.insertBefore(h,h1);
+function addlist(e){
+    e.preventDefault();
+let txt =document.querySelector('#item');
+let li=document.createElement('li');
+li.className='list-group-item';
+li.appendChild(document.createTextNode(txt.value));
+let btn=document.createElement('button');
+btn.className='btn btn-danger btn-sm float-right delete';
+btn.appendChild(document.createTextNode('X'));
+li.appendChild(btn);
+
+let btn2=document.createElement('button');
+btn2.className='btn btn-success btn-sm float-right delete';
+btn2.appendChild(document.createTextNode('edit'));
+li.appendChild(btn2);
+
+ulist.appendChild(li);  
+}
+
+function removelist(e){
+    e.preventDefault();
+    
+if(e.target.classList.contains('delete')){
+    if(confirm('Are you sure?')){
+        ulist.removeChild(e.target.parentElement);
+    }}
+}
