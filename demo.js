b0bd8fd2441a34cddf44fe7f50e38b2227ea401a
5Knight2 <pratiktarale258@gmail.com>
2023-05-02 12:01:17 +0530

    code

--- demo.js
+++ demo.js
@@ -1,6 +1,10 @@
 let header=document.getElementById('header-title');
 header.style.borderBottom='3px solid';
 
-let item=document.getElementsByClassName('title');
-item[0].style.color='green';
-item[0].style.fontWeight='bold';
+let titles=document.getElementsByClassName('title');
+titles[0].style.color='green';
+titles[0].style.fontWeight='bold';
+
+let items=document.getElementsByClassName('list-group-item');
+for(let i=0;i<items.length;i++)items[i].style.fontWeight='bold';
+items[2].style.backgroundColor='green';
